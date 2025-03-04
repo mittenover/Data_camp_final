4c12c7ab9a7dbd38e46356fd56c01ce0f07040db
Emma De Charry <emmadecharry@MacBook-Pro-de-Emma.local>
2025-03-04 14:02:53 +0100

    modif preprocess et download_data

--- download_data.py
+++ download_data.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 import opendatasets as od
-from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
     # Download the data
@@ -9,9 +8,3 @@ if __name__ == '__main__':
 
     od.download(dataset,data_dir='./data/')
     df = pd.read_csv('./data/stress-detection-dataset/stress_detection.csv')
-    
-    #split data into train and test
-
-    X_train, X_test = train_test_split(df, test_size=0.2, random_state=42)
-    X_train.to_csv('./data/stress-detection-dataset/train.csv', index=False)
-    X_test.to_csv('./data/stress-detection-dataset/test.csv', index=False)
--- preprocess.py
+++ preprocess.py
@@ -1,45 +1,26 @@
-#%%
 import pandas as pd
 import numpy as np
 import os
 from sklearn.model_selection import train_test_split
 
-file_path = "./data/student_health_data.csv"
-df = pd.read_csv(file_path)
-print(df.head())
-#%%
 def categorize_stress(level):
-    if 0 <= level < 2.5:
-        return "Not Very Stressed"
-    elif 2.5 <= level < 5:
-        return "Stressed"
-    elif 5 <= level < 7.5:
-        return "Very Stressed"
-    elif 7.5 <= level <= 10:
-        return "Highly Stressed"
+    if 0 <= level < 14:
+        return "Low Stress"
+    elif 14 <= level < 27:
+        return "Moderate Stress"
+    elif 27 <= level < 40:
+        return "High Perceived Stress"
     else:
         return "Unknown"
-# %%
-# Appliquer la catégorisation
-df["Stress_Category"] = df["Stress_Level_Biosensor"].apply(categorize_stress)
+    
+if __name__ == '__main__':
 
-# Sauvegarder le fichier prétraité
-df.to_csv("student_health_data_preprocessed.csv", index=False)
-# %%
-print(df.head())
-# %%
-# Vérifier les valeurs manquantes
-missing_values = df.isnull().sum()
-print("Missing values in each column:")
-print(missing_values)
-# %%
-# Charger le fichier prétraité
-df = pd.read_csv("student_health_data_preprocessed.csv")
-
-# Diviser les données en ensembles d'entraînement et de test
-train, test = train_test_split(df, test_size=0.2, random_state=42, stratify=df["Stress_Level_Biosensor"])
-
-# Sauvegarder les ensembles d'entraînement et de test
-train.to_csv("train.csv", index=False)
-test.to_csv("test.csv", index=False)
-# %%
+    file_path = "./data/stress-detection-dataset/stress_detection.csv"
+    df = pd.read_csv(file_path)
+    df["Stress_Category"] = df["PSS_score"].apply(categorize_stress)
+    df.to_csv("./data/stress-detection-dataset/preprocessed_stress_detection.csv", index=False)
+    df1 = pd.read_csv("./data/stress-detection-dataset/preprocessed_stress_detection.csv")
+    train, test = train_test_split(df1, test_size=0.2, random_state=42)
+    train.to_csv("./data/stress-detection-dataset/train.csv", index=False)
+    test.to_csv("./data/stress-detection-dataset/test.csv", index=False)
+    print("Preprocessing done.")
